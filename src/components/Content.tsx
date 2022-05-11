4edc9c3d2a07dbde55b9f8e20576dabec18676ba
Ivan Gomes <ivanjunior.pt@gmail.com>
2022-05-11 11:31:57 -0300

    modifying application

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,12 +1,31 @@
-import { MovieProps } from "../App"
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
 import { MovieCard } from "./MovieCard"
 
 interface ContentProps {
-  movies: MovieProps[]
+  selectedGenreId: number;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
 }
 
 
-export function Content({ movies }: ContentProps) {
+export function Content({ selectedGenreId}: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId])
 
   return (
     <div className="movies-list">
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,24 +1,44 @@
-import { GenreResponseProps } from "../App"
 import { Button } from "./Button"
+import '../styles/sidebar.scss';
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
 
 interface SidebarProps {
   genres: GenreResponseProps[];
   selectedGenreId: number;
-  handleClickButton: (id: number ) => void;
+  handleClickButton: (id: number) => void;
 }
 
-export function SideBar({ genres, selectedGenreId, handleClickButton }: SidebarProps) {
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({ selectedGenreId, handleClickButton }: SidebarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
   return (
-    <div className="buttons-container">
-      {genres.map(genre => (
-        <Button
-          key={String(genre.id)}
-          title={genre.title}
-          iconName={genre.name}
-          onClick={() => handleClickButton(genre.id)}
-          selected={selectedGenreId === genre.id}
-        />
-      ))}
-    </div>
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
   )
 }
